7821b1da9f045da36a9c30a657cc14e50ff4f8b7
JNY <jianningyuan@126.com>
2022-07-14 23:45:46 +0800

    BD写道可以该文件夹名，不可以替换非法字符，不可以下载多P视频，基本功能已完成

--- BD.py
+++ BD.py
@@ -1,4 +1,16 @@
-from os import system,listdir,path
+from os import system,listdir,path,rename
+
+def getFileName1(lpath,suffix):
+    # 获取指定目录下的所有指定后缀的文件名 
+    input_template_All=[]
+    f_list = listdir(lpath)#返回文件名
+    for i in f_list:
+        # path.splitext():分离文件名与扩展名
+        if path.splitext(i)[1] ==suffix:
+           input_template_All.append(path.splitext(i)[0])
+        #print(i)
+    return input_template_All
+
 para1 = input("请输入要下载视频的BV号：")
 para2 = input("请输入下载视频的P数(如8 或1,2 或3-5 或ALL):")
 try:
@@ -12,6 +24,11 @@ for k in range(len(Files)):
 
 Str2=['.mp4']
 if len(list(set(Str2).intersection(set(Files))))==len(Str2):
-    print("0")
+    #有MP4
+    fileName=getFileName1("D:\BilibiliDownloadFile\Temporary",".mp4")
+    fileName=fileName[0]
+    #print(fileName)
+    rename("D:\BilibiliDownloadFile\Temporary","D:\BilibiliDownloadFile\\"+fileName)
 else:
-    print("1")
+    #无mp4
+    print("0")
