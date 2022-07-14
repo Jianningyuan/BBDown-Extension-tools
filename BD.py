cb59113058fe8e02f3c76d421f4e4165e3a4d513
JNY <jianningyuan@126.com>
2022-07-15 00:25:34 +0800

    非法字符不行，多p下载只写了一个逻辑，新增pasparmu

--- BD.py
+++ BD.py
@@ -13,22 +13,29 @@ def getFileName1(lpath,suffix):
 
 para1 = input("请输入要下载视频的BV号：")
 para2 = input("请输入下载视频的P数(如8 或1,2 或3-5 或ALL):")
-try:
-    rnp = "%s \"%s\" \"%s\""%("D:\BilibiliDownloadTools\paspar.bat",para1, para2)#给paspar.bat传BV号及P数
-    system(rnp)
-except Exception as e:
-    print(e)
-Files=listdir("D:\BilibiliDownloadFile\Temporary")
-for k in range(len(Files)):
-    Files[k]=path.splitext(Files[k])[1]# 提取文件夹内所有文件的后缀
+if "," not in para2:
+    try:
+        rnp = "%s \"%s\" \"%s\" \"%s\""%("D:\BilibiliDownloadTools\paspar.bat",para1, para2,"D:\BilibiliDownloadFile\Temporary")#给paspar.bat传BV号及P数
+        system(rnp)
+    except Exception as e:
+        print(e)
+    Files=listdir("D:\BilibiliDownloadFile\Temporary")
+    for k in range(len(Files)):
+        Files[k]=path.splitext(Files[k])[1]# 提取文件夹内所有文件的后缀
 
-Str2=['.mp4']
-if len(list(set(Str2).intersection(set(Files))))==len(Str2):
-    #有MP4
-    fileName=getFileName1("D:\BilibiliDownloadFile\Temporary",".mp4")
-    fileName=fileName[0]
-    #print(fileName)
-    rename("D:\BilibiliDownloadFile\Temporary","D:\BilibiliDownloadFile\\"+fileName)
+    Str2=['.mp4']
+    if len(list(set(Str2).intersection(set(Files))))==len(Str2):
+        #有MP4
+        fileName=getFileName1("D:\BilibiliDownloadFile\Temporary",".mp4")
+        fileName=fileName[0]
+        #print(fileName)
+        rename("D:\BilibiliDownloadFile\Temporary","D:\BilibiliDownloadFile\\"+fileName)
+    # else:
+    #     #无mp4
+    #     print("0")
 else:
-    #无mp4
-    print("0")
+    try:
+        rnp1 = "%s \"%s\" \"%s\" \"%s\""%("D:\BilibiliDownloadTools\pasparmu.bat",para1, para2,"D:\BilibiliDownloadFile")
+        system(rnp1)
+    except Exception as e1:
+        print(e1)
---
+++ pasparmu.bat
@@ -0,0 +1,3 @@
+set bbdown_path=D:\BilibiliDownloadTools\BBDown_v1.4.8_20220127_win-x64
+cd D:\BilibiliDownloadFile
+%bbdown_path%\BBDown.exe %1 -p %2 --work-dir %3
