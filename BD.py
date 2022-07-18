a91153fde8580200ffefc3a9f03e91d314c33e54
JNY <jianningyuan@126.com>
2022-07-18 11:34:25 +0800

    压缩没写好，打算写多p单p判断

--- BD.py
+++ BD.py
@@ -1,4 +1,6 @@
+from fileinput import filename
 from os import system,listdir,path,rename
+from re import sub
 
 def getFileName1(lpath,suffix):
     # 获取指定目录下的所有指定后缀的文件名 
@@ -11,9 +13,22 @@ def getFileName1(lpath,suffix):
         #print(i)
     return input_template_All
 
+
+def validateTitle(title):
+	rstr = r"[\/\\\:\*\?\"\<\>\|]" # '/ \ : * ? " < > |'
+	new_title = sub(rstr, "_", title) # 替换为下划线
+	return new_title
+
+
 para1 = input("请输入要下载视频的BV号：")
 para2 = input("请输入下载视频的P数(如8 或1,2 或3-5 或ALL):")
-if "," not in para2:
+if "," in para2 or "-" in para2 or "ALL" in para2:
+    try:
+        rnp1 = "%s \"%s\" \"%s\" \"%s\""%("D:\BilibiliDownloadTools\pasparmu.bat",para1, para2,"D:\BilibiliDownloadFile")
+        system(rnp1)
+    except Exception as e1:
+        print(e1)
+else:
     try:
         rnp = "%s \"%s\" \"%s\" \"%s\""%("D:\BilibiliDownloadTools\paspar.bat",para1, para2,"D:\BilibiliDownloadFile\Temporary")#给paspar.bat传BV号及P数
         system(rnp)
@@ -28,14 +43,18 @@ if "," not in para2:
         #有MP4
         fileName=getFileName1("D:\BilibiliDownloadFile\Temporary",".mp4")
         fileName=fileName[0]
-        #print(fileName)
+        fileName=validateTitle(fileName)
         rename("D:\BilibiliDownloadFile\Temporary","D:\BilibiliDownloadFile\\"+fileName)
-    # else:
-    #     #无mp4
-    #     print("0")
-else:
-    try:
-        rnp1 = "%s \"%s\" \"%s\" \"%s\""%("D:\BilibiliDownloadTools\pasparmu.bat",para1, para2,"D:\BilibiliDownloadFile")
-        system(rnp1)
-    except Exception as e1:
-        print(e1)
+        fileNamefor7zroot="C:\\Program\000Files\\7-Zip\\7z.exe"
+        fileNamefor7z="D:\\BilibiliDownloadFile\\"+fileName+"\\"+fileName+"DanmakuAndSubtitles.7z"
+        fileNameforass="D:\\BilibiliDownloadFile\\"+fileName+"\\*.ass" + " D:\\BilibiliDownloadFile\\"+fileName+"\\*.xml"
+        print(fileNamefor7zroot)
+        print(fileNamefor7z)
+        print(fileNameforass)
+
+        #fileNameforxml=
+        try:
+            yasuo="%s \"%s\" \"%s\" \"%s\" \"%s\" \"%s\" \"%s\""%(fileNamefor7zroot,"a","-t7z",fileNamefor7z,"-mx=9","-mhe=on",fileNameforass)#,fileNameforxml)
+            system(yasuo)
+        except Exception as e2:
+            print(e2)
