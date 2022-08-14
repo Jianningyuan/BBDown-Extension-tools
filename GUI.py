1a8b1358fbdf8abfa4696de0dc9149385ea23691
JNY <jianningyuan@126.com>
2022-08-14 15:47:06 +0800

    增加容错，稳定性提升没有ini

--- GUI.py
+++ GUI.py
@@ -18,6 +18,50 @@ ScaleFactor=ctypes.windll.shcore.GetScaleFactorForDevice(0)
 win.tk.call('tk', 'scaling', ScaleFactor/40)
 
 
+def SplitTextAndCompareNumber(text,splitChar,compareNumber):
+    splitTextArr=str(text).split(str(splitChar))
+    for i in splitTextArr:
+        if i=="":
+            return "No"
+    resultOfsplitText=splitTextArr[len(splitTextArr)-1]
+    if int(resultOfsplitText)<=int(compareNumber):
+        return True
+    else:
+        return False
+
+
+def VerifyThatTheInputIsLegitimateOfP(legitimateOfP):
+    text=entryOfP.get()
+    if text:
+        try:
+            int(text)
+            if text<=legitimateOfP:
+                return True
+            else:
+                return False
+        except Exception as e:
+            if "," in text or "-" in text or "ALL" in text or "all" in text:
+                listOfPOrd=["48","49","50","51","52","53","54","55","56","57","45","44","65","97","76","108"] # 分别为 0，1，2，3，4，5，6，7，8，9，“-”，“,”,A,a,L,l(均为str)
+                for i in text:
+                    if str(ord(i)) in listOfPOrd:
+                        pass
+                    else:
+                        # print(type(ord(i)))
+                        return False
+                if "," in text:
+                    #查看“，”后的数值是否小于等于legitimate
+                    resultOfDouHao=SplitTextAndCompareNumber(text,",",legitimateOfP)      # 返回值合法为Ture，不合法为False
+                    return resultOfDouHao
+                elif "-" in text:
+                    #查看“-”后的数值是否小于等于legitimate
+                    resultOfHengGang=SplitTextAndCompareNumber(text,"-",legitimateOfP)    # 返回值合法为Ture，不合法为False
+                    return resultOfHengGang
+            else:
+                return False
+    else:
+        return False
+
+
 def ThreadsOfDownload1P(BV):
         t1 = Thread(target=DownLoad, args=(BV,1))
         t1.start()
@@ -37,29 +81,42 @@ def RunShow(self):
 def ThreadOfDownloadMultiP():
     BV=EntryOfPGet(entry)
     P=EntryOfPGet(entryOfP)
-    tOfMulti=Thread(target=DownLoad, args=(BV,P))
-    tOfMulti.start()
-    win.destroy()
-    tOfMulti.join()
+    if VerifyThatTheInputIsLegitimateOfP(ngp(BV))==True:
+        tOfMulti=Thread(target=DownLoad, args=(BV,P))
+        tOfMulti.start()
+        win.destroy()
+        tOfMulti.join()
+    elif VerifyThatTheInputIsLegitimateOfP(ngp(BV))=="No":
+        showerror("警告","P数输入有误!")
+    else:
+        showerror("警告","P数超出范围!")
 
+
+def RunThreadOfDownloadMultiP(self):
+    ThreadOfDownloadMultiP()
 
 def Show():
     strbvid=entry.get()
-    yesAndNoOfVideo=DownLoadInit(strbvid)
-    if str(yesAndNoOfVideo) == "0":   #有无视频
-        if ngp(strbvid) == "1":       #是否为1p
-            ThreadsOfDownload1P(strbvid)
+    if str(len(strbvid))=="12":
+        yesAndNoOfVideo=DownLoadInit(strbvid)
+        if str(yesAndNoOfVideo) == "0":   #有无视频
+            if ngp(strbvid) == "1":       #是否为1p
+                ThreadsOfDownload1P(strbvid)
+            else:
+                entry.config(state='disable')
+                Butt.destroy()
+                Label(win,text="请输入下载视频的P数(如8 或1,2 或3-5 或ALL)").grid(row=1,column=0)
+                entryOfP.grid(row=2,column=0)
+                bu1.grid(row=3,column=0)
         else:
-            Butt.destroy()
-            Label(win,text="请输入下载视频的P数(如8 或1,2 或3-5 或ALL)").grid(row=1,column=0)
-            entryOfP.grid(row=2,column=0)
-            bu1.grid(row=3,column=0)
+            showerror("错误","视频不存在!")
     else:
-        showerror("错误","视频不存在!")
+        showerror("警告","BV号不正确!")
 
 
 bu1=Button(win,text="下载",command=ThreadOfDownloadMultiP)
 entryOfP=Entry(win)
+entryOfP.bind("<Return>",RunThreadOfDownloadMultiP)
 entry=Entry(win)
 entry.grid(row=0)
 Butt=Button(win,text="下载",command=Show)
